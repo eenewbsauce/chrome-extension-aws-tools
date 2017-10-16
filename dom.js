f1df4fc3b68c3f9e9bac25bb41e221c6040f8af8
eenewbsauce <ryan.m.tracey@gmail.com>
2017-10-16 15:08:23 -0400

    feat(search): enter to search

--- dom.js
+++ dom.js
@@ -1,21 +1,52 @@
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-	//refresh loan
-	var b = document.getElementById('ddbv2-scan-query-start-button');
+chrome.extension.onMessage
+	.addListener(
+		function(request, sender, sendResponse) {
+			searchLoan(doTheRest);
+		}
+	)	
 
+function doTheRest() {
+	closeLoan();
+	openLoan();
+}
+
+function closeLoan() {
+	document.querySelector('.mcancel').dispatchEvent(new MouseEvent('click'));
+	killSaveListener();
+}
+
+function searchLoan(cb, timeout) {
+	if (typeof timeout === 'undefined') {
+		timeout = 1500;
+	}
+
+	var b = document.getElementById('ddbv2-scan-query-start-button');
 	b.click()
 
-	setTimeout(doTheRest, 1500);
+	setTimeout(cb, timeout);
+}
 
-	function doTheRest() {
-		//close modal
-		document.querySelector('.mcancel').dispatchEvent(new MouseEvent('click'));
+function openLoan() {
+	var a = document.querySelectorAll('#ddbv2-items-view table tr td a');
+	a[7].click();
+	listenForSave();
+}
 
-		//open modal again
-		var a = document.querySelectorAll('#ddbv2-items-view table tr td a');
+function listenForSave() {
+	$('#cmn-confirm-btn')
+		.on('click', function() {
+			searchLoan(openLoan);
+		});
+}
 
-		a[7].click();
+function killSaveListener() {
+	$('#cmn-confirm-btn').off('click');
+}
 
-    	sendResponse({ success: true });
-	}
-});
+//handle enter key for search
+$(document)
+	.on('keydown', function(e) {
+		if (e.keyCode === 13) {
+			searchLoan(openLoan, 3000);
+		}
+	});
